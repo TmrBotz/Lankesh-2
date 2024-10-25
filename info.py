1ab93656262562281c0a26db3599e2b181fbf015
TmrBotz <tmrbotz@gmail.com>
2024-10-25 13:44:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,7 +69,7 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-START_IMG = (environ.get('START_IMG', 'https://i.ibb.co/qpxpGmC/image.jpg https://i.ibb.co/DQ35zLZ/image.jpg')).split()
+START_IMG = (environ.get('START_IMG', 'https://envs.sh/Wra.jpg https://envs.sh/WrO.jpg https://envs.sh/Wrm.jpg https://envs.sh/WrM.jpg https://envs.sh/WrH.jpg https://envs.sh/Wrg.jpg https://envs.sh/Wrf.jpg')).split()
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://graph.org/file/f4db1c3ad3d9e38b328e6.jpg')).split()
